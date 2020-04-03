88a40ded8e918984a510b11228729a21ca188b13
Dugi97 <lazardugalic1@gmail.com>
2020-04-03 17:07:31 +0200

    database setup

--- src/Core/Database/DatabaseConfig.php
+++ src/Core/Database/DatabaseConfig.php
@@ -5,7 +5,7 @@ namespace MVC\Core\Database;
 class DatabaseConfig
 {
     const DB_HOST = "localhost";
-    const DB_NAME = "MVC";
+    const DB_NAME = "test";
     const DB_PASSWORD = "";
     const DB_USER  = "root";
 }
